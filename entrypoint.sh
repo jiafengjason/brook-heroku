bf41b73955e1e829b454f003ad0c8bc3529f534c
jiafengjason <jiafengjason@163.com>
2021-07-20 20:27:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 curl -L https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -o /usr/bin/brook
 chmod +x /usr/bin/brook
 
-./brook --help
-echo "Hello,the world!"
+brook --help
+echo "Hello world!"
 
-./brook ${COMMAND}
+brook ${COMMAND}

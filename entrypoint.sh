1dd0b12c671ba034d6d514bfec7eb6144b69960e
jiafengjason <jiafengjason@163.com>
2021-07-21 11:23:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 curl -L https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -o /usr/bin/brook
-chmod +x /usr/bin/brook
+chmod +x /brook/brook
 
 brook --help
 
-COMMAND="brook wsserver -l :$PORT -p $PASSWORD"
+COMMAND="/brook/brook wsserver -l :$PORT -p $PASSWORD"
 echo ${COMMAND}
 
 ${COMMAND} &
